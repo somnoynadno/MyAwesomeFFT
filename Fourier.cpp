8c59841dc3df23f647269f1e501286c617f150f1
somnoynadno <somnoynadno@yandex.ru>
2019-10-23 23:28:39 +0700

    Chrono added

--- Fourier.cpp
+++ Fourier.cpp
@@ -2,6 +2,7 @@
 #define CL_HPP_TARGET_OPENCL_VERSION 200
 
 #include <bits/stdc++.h> // for fft
+#include <chrono> // to work with time
 #include <x86intrin.h> // for vectorization
 #include <omp.h> // OpenMP, obviously
 #include <CL/cl2.hpp> // OpenCL, obviously
@@ -15,7 +16,7 @@ g++ Fourier.cpp -o Fourier -pthread -l OpenCL && ./Fourier
 
 typedef complex<double> ftype;
 const double pi = acos(-1);
-const int maxn = 1024;
+const int maxn = 1024*16*16*4;
 ftype w[maxn];
 
 // generate roots of -1
@@ -52,8 +53,8 @@ void omp_fft(T *in, ftype *out, int n, int k = 1) {
     n >>= 1;
 #pragma omp parallel
     {
-        fft(in, out, n, 2 * k);
-        fft(in + k, out + n, n, 2 * k);
+        omp_fft(in, out, n, 2 * k);
+        omp_fft(in + k, out + n, n, 2 * k);
     }
     for(int i = 0, j = 0; i < n; i++, j += t) {
         ftype t = w[j] * out[i + n];
@@ -72,26 +73,45 @@ signed main() {
     vector <float> input; 
 
     // to generate random signal
-    // for (int i = 0; i < n; i++){
-    //     // r = random(0, 1)
-    //     float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-    //     input.push_back(r);
+    for (int i = 0; i < n; i++){
+        // r = random(0, 1)
+        float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        input.push_back(r);
+    }
+
+    // for (int i = 0; i < 4*64; i++){
+    //     input.push_back(0);
     // }
 
-    for (int i = 0; i < 4*64; i++){
-        input.push_back(0);
-    }
+    // for (int i = 0; i < 8*64; i++){
+    //     input.push_back(1);
+    // }
 
-    for (int i = 0; i < 8*64; i++){
-        input.push_back(1);
-    }
+    // for (int i = 0; i < 4*64; i++){
+    //     input.push_back(0);
+    // }
 
-    for (int i = 0; i < 4*64; i++){
-        input.push_back(0);
-    }
+    cout << "Current number of elements is " << n << endl;
 
     vector<ftype> inv(n);
-    omp_fft(input.data(), inv.data(), n);
+    cout << "Start FFT" << endl;
+
+    chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+    fft(input.data(), inv.data(), n);
+    chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+
+    auto duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
+    cout << "FFT ended with " << duration << " ms" << endl;
+
+    vector<ftype> inv2(n);
+    cout << "Start FFT with OpenMP" << endl;
+
+    t1 = chrono::high_resolution_clock::now();
+    omp_fft(input.data(), inv2.data(), n);
+    t2 = chrono::high_resolution_clock::now();
+
+    duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
+    cout << "FFT with OpenMP ended with " << duration << " ms" << endl;
 
     // for (int i = 0; i < n; i++){
     //     cout << inv[i] << endl;
