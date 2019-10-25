e916fa73730ae03c330d8342ad6d7ca5108e0e9b
somnoynadno <somnoynadno@yandex.ru>
2019-10-26 01:09:57 +0700

    Pure C version of FFT

--- Fourier.cpp
+++ Fourier.cpp
@@ -1,7 +1,7 @@
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_TARGET_OPENCL_VERSION 200
 
-#define ITER_NUM 10
+#define ITER_NUM 100
 
 #include <bits/stdc++.h> // for fft
 #include <chrono> // to work with time
@@ -19,95 +19,154 @@ g++ Fourier.cpp -o Fourier -pthread -l OpenCL -mssse3 -fopenmp && ./Fourier
 */
 
 typedef complex<double> ftype;
+typedef struct { double r; double i; } w_type;
 const double pi = acos(-1);
-const int maxn = 1024*16*16;
-ftype w[maxn];
+const int maxn = 1024*16*2;
+// ftype w[maxn];
+w_type w_new[maxn];
 
 // generate roots of -1
 void init() {
     for(int i = 0; i < maxn; i++) {
-        w[i] = polar(1., 2 * pi / maxn * i);
+        ftype w = polar(1., 2 * pi / maxn * i);
+        w_new[i].r = w.real();
+        w_new[i].i = w.imag();
     }
 }
  
-template<typename T>
-void fft(T *in, ftype *out, int n, int k = 1) {
-    if(n == 1) {
-        *out = *in;
+// template<typename T>
+// void fft(T *in, ftype *out, int n, int k = 1) {
+//     if(n == 1) {
+//         *out = *in;
+//         return;
+//     }
+//     int t = maxn / n;
+//     n >>= 1;
+//     fft(in, out, n, 2 * k);
+//     fft(in + k, out + n, n, 2 * k);
+//     for(int i = 0, j = 0; i < n; i++, j += t) {
+//         ftype t = w[j] * out[i + n];
+//         out[i + n] = out[i] - t;
+//         out[i] += t;
+//     }
+// }
+
+// template<typename T>
+// void omp_fft(T *in, ftype *out, int n, int k = 1) {
+//     if(n == 1) {
+//         *out = *in;
+//         return;
+//     }
+//     n >>= 1;
+
+//     fft(in, out, n, 2 * k);
+//     fft(in + k, out + n, n, 2 * k);
+
+//     const int t1 = maxn / n;
+//     int j = 0;
+
+// #pragma omp parallel num_threads(4) 
+//     {
+//     #pragma omp for
+//         for (int i = 0; i < n; i++) {
+//             ftype t = w[j] * out[i + n];
+//             out[i + n] = out[i] - t;
+//             out[i] += t;
+//         #pragma omp atomic
+//             j += t1;
+//         }
+//     }
+// }
+
+// // complex numbers multiplication
+// static __inline__ __m128d ZMUL(__m128d a, __m128d b){
+//     __m128d ar, ai;
+
+//     ar = _mm_movedup_pd(a);
+//     ar = _mm_mul_pd(ar, b);
+//     ai = _mm_unpackhi_pd(a, a);
+//     b = _mm_shuffle_pd(b, b, 1);
+//     ai = _mm_mul_pd(ai, b);
+
+//     return _mm_addsub_pd(ar, ai);
+// }
+
+
+// void fft_with_vec(double *in, __m128d *out, int n, int k = 1) {
+//     if(n == 1) {
+//         *out = _mm_set_sd(*in);
+//         return;
+//     }
+//     int t = maxn / n;
+//     n >>= 1;
+//     fft_with_vec(in, out, n, 2 * k);
+//     fft_with_vec(in + k, out + n, n, 2 * k);
+
+//     __m128d w0, res;
+
+//     for (int i = 0, j = 0; i < n; i++, j += t) {
+//         w0 = _mm_set_pd(w_new[j].i, w_new[j].r);
+//         res = ZMUL(out[i+n], w0);
+
+//         out[i + n] = _mm_sub_pd(out[i], res);
+//         out[i] += res;
+//     }
+// }
+
+w_type complex_mult(w_type a, w_type b){
+    w_type c;
+    c.r = a.r*b.r - a.i*b.i;
+    c.i = a.i*b.r + a.r*b.i;
+    return c;
+}
+
+void fft_c(double *in, w_type *out, int n, int k = 1){
+    if (n == 1){
+        out[0].r = in[0];
+        out[0].i = 0;
         return;
     }
+
     int t = maxn / n;
     n >>= 1;
-    fft(in, out, n, 2 * k);
-    fft(in + k, out + n, n, 2 * k);
-    for(int i = 0, j = 0; i < n; i++, j += t) {
-        ftype t = w[j] * out[i + n];
-        out[i + n] = out[i] - t;
-        out[i] += t;
-    }
-}
-
-template<typename T>
-void omp_fft(T *in, ftype *out, int n, int k = 1) {
-    if(n == 1) {
-        *out = *in;
-        return;
-    }
-    n >>= 1;
-
-    omp_fft(in, out, n, 2 * k);
-    omp_fft(in + k, out + n, n, 2 * k);
-
-    const int t1 = maxn / n;
-    int j = 0;
-
-#pragma omp parallel num_threads(2) 
-    {
-    #pragma omp for
-        for (int i = 0; i < n; i++) {
-            ftype t = w[j] * out[i + n];
-            out[i + n] = out[i] - t;
-            out[i] += t;
-        #pragma omp atomic
-            j += t1;
-        }
-    }
-}
-
-// complex numbers multiplication
-static __inline__ __m128d ZMUL(__m128d a, __m128d b){
-    __m128d ar, ai;
-
-    ar = _mm_movedup_pd(a);
-    ar = _mm_mul_pd(ar, b);
-    ai = _mm_unpackhi_pd(a, a);
-    b = _mm_shuffle_pd(b, b, 1);
-    ai = _mm_mul_pd(ai, b);
-
-    return _mm_addsub_pd(ar, ai);
-}
-
-
-void fft_with_vec(double *in, __m128d *out, int n, int k = 1) {
-    if(n == 1) {
-        *out = _mm_set_sd(*in);
-        return;
-    }
-    int t = maxn / n;
-    n >>= 1;
-    fft_with_vec(in, out, n, 2 * k);
-    fft_with_vec(in + k, out + n, n, 2 * k);
-
-    __m128d w0, res;
+    fft_c(in, out, n, 2 * k);
+    fft_c(in + k, out + n, n, 2 * k);
 
     for (int i = 0, j = 0; i < n; i++, j += t) {
-        w0 = _mm_set_pd(w[j].imag(), w[j].real());
-        res = ZMUL(out[i+n], w0);
-
-        out[i + n] = _mm_sub_pd(out[i], res);
-        out[i] += res;
+        w_type res = complex_mult(w_new[j], out[i + n]);
+        out[i + n].r = out[i].r - res.r;
+        out[i].r += res.r;
+        out[i + n].i = out[i].i - res.i;
+        out[i].i += res.i;
     }
 }
+
+void fft_c_parallel(double *in, w_type *out, int n, int k = 1){
+    if (n == 1){
+        out[0].r = in[0];
+        out[0].i = 0;
+        return;
+    }
+
+    int t = maxn / n;
+    n >>= 1;
+    fft_c(in, out, n, 2 * k);
+    fft_c(in + k, out + n, n, 2 * k);
+
+    int j = 0;
+
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < n; i++) {
+        w_type res = complex_mult(w_new[j], out[i + n]);
+        out[i + n].r = out[i].r - res.r;
+        out[i].r += res.r;
+        out[i + n].i = out[i].i - res.i;
+        out[i].i += res.i;
+    #pragma omp atomic
+        j += t;
+    }
+}
+
 
 void printMyPlatforms();
 
@@ -118,13 +177,13 @@ int main() {
     init();
 
     int n = maxn;
-    vector <double> input; 
+    double input[maxn]; 
 
     // to generate random signal
     for (int i = 0; i < n; i++){
         // r = random(0, 1000)
         double r = rand() % 1000;
-        input.push_back(r);
+        input[i] = r;
     }
 
     // n = 1024;
@@ -147,46 +206,72 @@ int main() {
     cout << "Current number of elements is " << n << endl;
     cout << "Current number of iteration is " << ITER_NUM << endl;
 
-    vector<ftype> inv(n);
-    cout << "Start FFT" << endl;
+    // vector<ftype> inv(n);
+    // cout << "Start FFT" << endl;
     
-    for (int i = 0; i < ITER_NUM; i++){
-        t1 = chrono::high_resolution_clock::now();
-        fft(input.data(), inv.data(), n);
-        t2 = chrono::high_resolution_clock::now();
+    // for (int i = 0; i < ITER_NUM; i++){
+    //     t1 = chrono::high_resolution_clock::now();
+    //     fft(input.data(), inv.data(), n);
+    //     t2 = chrono::high_resolution_clock::now();
 
-        auto duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
-        t += duration;
-    }
-    cout << "FFT ended with " << t/ITER_NUM << " ms" << endl;
+    //     auto duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
+    //     t += duration;
+    // }
+    // cout << "FFT ended with " << t << " ms" << endl;
+
+    // t = 0;
+    // cout << "Start FFT with OpenMP" << endl;
+
+    // for (int i = 0; i < ITER_NUM; i++){
+    //     t1 = chrono::high_resolution_clock::now();
+    //     omp_fft(input.data(), inv.data(), n);
+    //     t2 = chrono::high_resolution_clock::now();
+
+    //     auto duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
+    //     t += duration;
+    // }
+    // cout << "FFT with OpenMP ended with " << t << " ms" << endl;
+
+    // t = 0;
+    // __m128d inv3[n];
+    // cout << "Start FFT with vectorization" << endl;
+
+    // for (int i = 0; i < ITER_NUM; i++){
+    //     t1 = chrono::high_resolution_clock::now();
+    //     fft_with_vec(input.data(), inv3, n);
+    //     t2 = chrono::high_resolution_clock::now();
+
+    //     auto duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
+    //     t += duration;
+    // }
+    // cout << "FFT with vectorization ended with " << t << " ms" << endl;
 
     t = 0;
-    vector<ftype> inv2(n);
-    cout << "Start FFT with OpenMP" << endl;
+    w_type inv4[n];
+    cout << "Start pure C FFT" << endl;
 
     for (int i = 0; i < ITER_NUM; i++){
         t1 = chrono::high_resolution_clock::now();
-        omp_fft(input.data(), inv2.data(), n);
+        fft_c(input, inv4, n);
         t2 = chrono::high_resolution_clock::now();
 
         auto duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
         t += duration;
     }
-    cout << "FFT with OpenMP ended with " << t/ITER_NUM << " ms" << endl;
+    cout << "Pure C FFT ended with " << t << " ms" << endl;
+
 
     t = 0;
-    __m128d inv3[n];
-    cout << "Start FFT with vectorization" << endl;
-
+    cout << "Start parallel pure C FFT" << endl;
     for (int i = 0; i < ITER_NUM; i++){
         t1 = chrono::high_resolution_clock::now();
-        fft_with_vec(input.data(), inv3, n);
+        fft_c_parallel(input, inv4, n);
         t2 = chrono::high_resolution_clock::now();
 
         auto duration = chrono::duration_cast<chrono::milliseconds> (t2 - t1).count();
         t += duration;
     }
-    cout << "FFT with vectorization ended with " << t/ITER_NUM << " ms" << endl;
+    cout << "Parallel pure C FFT ended with " << t << " ms" << endl;
 
     // printMyPlatforms();
 
